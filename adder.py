1cd4bed21e6426d1b1e2992c62f293528220dcaa
root <kehlinswain@gmail.com>
2017-08-15 04:15:28 +0000

    adder script

---
+++ adder.py
@@ -0,0 +1,19 @@
+
+
+
+def adder():
+    s = 4
+    y = 3
+    x = s + y
+    return x
+
+#This function passes in parameters: it will append data to a list
+def appendNumbersToList(kehlin):
+    bob = [1,2,3,4]
+    bob.append(kehlin)
+    return bob
+    
+
+print appendNumbersToList("ice cream")
+
+print adder()
